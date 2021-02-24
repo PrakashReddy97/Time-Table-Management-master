974895d2bb6492797ce0dfd4ee17da1bb30f91e4
PrakashReddy97 <sanguprakashreddy@gmail.com>
2021-02-24 18:17:07 +0530

    Initial commit

---
+++ Time-Table-Management-master/src/Day/Days.java
@@ -0,0 +1,26 @@
+package Day;
+
+public class Days
+{
+
+    private int ID;
+    private String DayName;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getDayName() {
+        return DayName;
+    }
+
+    public void setDayName(String dayName) {
+        DayName = dayName;
+    }
+
+
+}
